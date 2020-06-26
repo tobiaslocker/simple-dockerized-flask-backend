4fd0670e9c340569a5fec87af6cf9c32b3316d0c
Tobias Locker <tobias@tobiaslocker.de>
2020-06-26 09:49:41 +0200

    clean up

--- app/api.py
+++ app/api.py
@@ -1,18 +1,14 @@
 from flask import Blueprint, make_response, jsonify
 from flask_cors import cross_origin
-
 import logging
 import json
-
 from flask import request
-
 from . import create_app, database
 from .models import Cats
 
-
 logging.basicConfig(level=logging.DEBUG)
-
 api = Blueprint('api', __name__)
+
 
 @api.route('/resource')
 @cross_origin()
